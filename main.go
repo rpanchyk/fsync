235b3bf0dc3f9142ec97a71d874c4d2a14a4eb0d
rpanchyk <ruslan.panchik@gmail.com>
2024-04-09 15:52:02 +0300

    Add basic flags

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	deleteFlag bool
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Usage = func() {
+		w := flag.CommandLine.Output()
+		fmt.Fprintf(w, "\nfsync is a file transfer program capable of local update. \nUsage: fsync [OPTION]... SRC [SRC]... DEST\n\n")
+		flag.PrintDefaults()
+	}
+
+	flag.BoolVar(&deleteFlag, "delete", false, "delete extraneous files from dest dirs")
+	flag.Parse()
+
+	fmt.Println("deleteFlag value is: ", deleteFlag)
+	fmt.Println("Non-flag arguments:", flag.Args())
 }
