53916abe148d07573f3d35411a35ab279bcf88a5
rpanchyk <ruslan.panchik@gmail.com>
2024-04-17 16:09:29 +0300

    Optimize check sums

--- internal/verify/md5_verifier.go
+++ internal/verify/md5_verifier.go
@@ -5,36 +5,56 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 type MD5Verifier struct {
 }
 
 func (v MD5Verifier) Same(file1, file2 string) (bool, error) {
-	sum1, err := v.getSum(file1)
-	if err != nil {
+	var wg sync.WaitGroup
+	outChan := make(chan string, 2)
+	errChan := make(chan error, 2)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		v.getSum(file1, outChan, errChan)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		v.getSum(file2, outChan, errChan)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(outChan)
+		close(errChan)
+	}()
+
+	for err := range errChan {
 		return false, err
 	}
 
-	sum2, err := v.getSum(file2)
-	if err != nil {
-		return false, err
-	}
-
-	return sum1 == sum2, nil
+	sumA := <-outChan
+	sumB := <-outChan
+	return sumA == sumB, nil
 }
 
-func (v MD5Verifier) getSum(file string) (string, error) {
-	f, err := os.Open(file)
+func (v MD5Verifier) getSum(filePath string, outChan chan string, errChan chan error) {
+	file, err := os.Open(filePath)
 	if err != nil {
-		return "", err
+		errChan <- err
+		return
 	}
-	defer f.Close()
+	defer file.Close()
 
-	h := md5.New()
-	if _, err := io.Copy(h, f); err != nil {
-		return "", err
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		errChan <- err
+		return
 	}
-
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
+	outChan <- fmt.Sprintf("%x", hash.Sum(nil))
 }
